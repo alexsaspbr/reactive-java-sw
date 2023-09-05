b558d226c9ea273aaeeec2348a5093cb4d733e58
Alex Araujo <alexsaspbr@gmail.com>
2023-09-04 22:02:24 -0300

    inicio aula spring web flux

---
+++ src/main/java/br/com/ada/reactivejavasw/TestRepository.java
@@ -0,0 +1,11 @@
+package br.com.ada.reactivejavasw;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TestRepository extends ReactiveMongoRepository<String, Long> {
+
+
+
+}
